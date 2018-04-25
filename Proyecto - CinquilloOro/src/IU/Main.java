35aa9ba6c55cedba97ada6adf1615b72afe0c602
Cosas de Puma <kikefontanlorenzo@gmail.com>
2018-04-25 14:35:08 +0200

    Author?
    Nota Aclarativa

--- Proyecto - CinquilloOro/src/IU/Main.java
+++ Proyecto - CinquilloOro/src/IU/Main.java
@@ -6,8 +6,21 @@ import static IU.Juego.inicioJuego;
  * 
  * @author Diego Enrique Fontán Lorenzo
  * @author Julio Patricio da Silva
+ * @author Gabriel Rodriguez Bouzan
+ * 
+ * < NOTA ACLARATIVA >
+ * A lo largo del código aparecen varias funciones operacionales.
+ * Dichos segmentos de código han sido generados mediante el autocorrector
+ * de Netbeans a partir de bucles for-each programados por nosotros.
+ * Dichos for-each están comentados justo en la parte superior de las
+ * funciones operacionales comentados, de tal manera que podamos saber cómo
+ * funcionan.
+ * La decisión de reemplazar los bucles por los sugeridos ha sido para evitar
+ * tener Warnings.
+ * 
+ * CUALQUIER PROBLEMA QUE PUEDA SURGIR, POR FAVOR, COMENTADLO CON NOSOTROS
+ * 
  */
-
 public class Main {
 
     /**
